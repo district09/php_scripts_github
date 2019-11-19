ebbbdf50707d32ca07f7d3761ed21b5c35d12794
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2019-11-19 08:00:51 +0100

    Improve autoloading.

--- bin/github.php
+++ bin/github.php
@@ -4,7 +4,27 @@
  */
 
 // Autoloader.
-require __DIR__ . '/../vendor/autoload.php';
+$autoloadLocations = [
+    getcwd() . '/vendor/autoload.php',
+    getcwd() . '/../../autoload.php',
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php',
+];
+$loaded = false;
+foreach ($autoloadLocations as $autoload) {
+    if (is_file($autoload)) {
+        require_once($autoload);
+        $loaded = true;
+    }
+}
+if (!$loaded) {
+    fwrite(STDERR,
+        'You must set up the project dependencies, run the following commands:' . PHP_EOL .
+        'curl -s http://getcomposer.org/installer | php' . PHP_EOL .
+        'php composer.phar install' . PHP_EOL
+    );
+    exit(1);
+}
 
 use DigipolisGent\Github\Application;
 use DigipolisGent\Github\Core\Command\Branch\ProtectCommand;
